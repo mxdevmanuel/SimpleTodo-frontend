6c65da4b705259eace7cea7aa2a6981580166bb5
Manuel Morales <p513manu@gmail.com>
2025-06-05 13:01:08 -0600

    change build url

--- src/stores/todoStore.js
+++ src/stores/todoStore.js
@@ -1,7 +1,7 @@
 import { defineStore } from 'pinia';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000';
+const API_URL = import.meta.env.API_URL || 'http://localhost:3000';
 
 export const useTodoStore = defineStore('todos', {
   state: () => ({
